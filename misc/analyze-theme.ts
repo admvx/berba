5008b51c2486d894d0c9580a6e26bd8e95b91c73
Adam Vernon <hello@admvx.com>
2022-06-21 22:58:18 -0700

    Add more detailed theme analysis / comparison

--- misc/analyze-theme.ts
+++ misc/analyze-theme.ts
@@ -13,6 +13,7 @@ ${themePath}
   let fileContent = file.toString('utf-8').replace(lineCommentsRegEx, '$1');
   let theme = JSON.parse(fileContent);
   
+  let themeInfo = new ThemeInfo();
   let colorMap: {[key: string]: ColorUsageList} = { };
   let uniqueColors = 0;
   
@@ -21,7 +22,7 @@ ${themePath}
     let opaqueColor = color.substring(0, 7);
     if (! colorMap[opaqueColor]) {
       uniqueColors ++;
-      colorMap[opaqueColor] = new ColorUsageList(opaqueColor);
+      colorMap[opaqueColor] = new ColorUsageList(opaqueColor, themeInfo);
     }
     colorMap[opaqueColor].addBasicUsage(key, color);
   }
@@ -32,7 +33,7 @@ ${themePath}
     let opaqueColor = color.substring(0, 7);
     if (! colorMap[opaqueColor]) {
       uniqueColors ++;
-      colorMap[opaqueColor] = new ColorUsageList(opaqueColor);
+      colorMap[opaqueColor] = new ColorUsageList(opaqueColor, themeInfo);
     }
     colorMap[opaqueColor].addTokenUsage(item);
   }
@@ -46,21 +47,58 @@ ${themePath}
     colorMap[color].sortUsages();
     colorMap[color].logDescription();
   }
+  
+  return themeInfo;
 };
+
+
+class ThemeInfo {
+  
+  public allColors: ColorUsageList[] = [];
+  public colorLookup: {[key: string]: string} = {};
+  public tokenLookup: {[key: string]: string} = {};
+  
+  addColor(color: ColorUsageList): void {
+    this.allColors.push(color);
+  }
+  
+  addBasicUsage(key: string, value: string): void {
+    this.colorLookup[key] = value;
+  }
+  
+  addTokenUsage(tokenBlock: any): void {
+    let color = tokenBlock.settings.foreground;
+    let scopes: string[];
+    
+    if (tokenBlock.scope.charAt) {
+      scopes = [tokenBlock.scope];
+    } else {
+      scopes = tokenBlock.scope;
+    }
+    
+    scopes.forEach(scope => {
+      this.tokenLookup[scope] = color;
+    });
+  }
+    
+}
+
 
 class ColorUsageList {
   
   private basicUsages: [string, string][] = [];
   private tokenUsages: any[] = [];
   
-  constructor(public opaqueColor: string) { }
+  constructor(public opaqueColor: string, private parentTheme: ThemeInfo) { }
   
   addBasicUsage(key: string, value: string): void {
     this.basicUsages.push([key, value]);
+    this.parentTheme.addBasicUsage(key, value);
   }
   
   addTokenUsage(tokenBlock: any): void {
     this.tokenUsages.push(tokenBlock);
+    this.parentTheme.addTokenUsage(tokenBlock);
   }
   
   sortUsages(): void {
@@ -73,7 +111,7 @@ class ColorUsageList {
     console.log(`\n"${this.opaqueColor}" (${this.count}):`);
     if (this.basicUsages.length) {
       for (let usage of this.basicUsages) {
-        console.log(`"${usage[0]}":, "${usage[1]}",`);
+        console.log(`"${usage[0]}": "${usage[1]}",`);
       }
     }
     if (this.tokenUsages.length) {
@@ -89,6 +127,19 @@ class ColorUsageList {
 
 
 (async function() {
-  await processTheme(mainPath);
-  await processTheme(comparisonPath);
+  let mainTheme = await processTheme(mainPath);
+  let referenceTheme = await processTheme(comparisonPath);
+  
+  Object.keys(referenceTheme.colorLookup).forEach(colorId => {
+    if (! mainTheme.colorLookup[colorId]) {
+      console.log('Could not find (color):', colorId);
+    }
+  });
+  
+  Object.keys(referenceTheme.tokenLookup).forEach(tokenId => {
+    if (! mainTheme.tokenLookup[tokenId]) {
+      console.log('Could not find (token):', tokenId);
+    }
+  });
+    
 })();
